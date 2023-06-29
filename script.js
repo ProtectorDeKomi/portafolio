6413f1be4549efab0a474167e0db63d830906017
Astartes <shoukomilhouse@gmail.com>
2023-06-28 20:42:31 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.cookie = "nombreCookie=valor; SameSite=Lax";
+
+window.addEventListener('scroll', function() {
+    var parallax = document.querySelector('.parallax');
+    var scrollPosition = window.pageYOffset;
+    parallax.style.transform = 'translateY(' + scrollPosition * 0.5 + 'px)';
+});
